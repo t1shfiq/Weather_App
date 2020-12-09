63b84e6b46ad82837a1843be550944dd92fbb425
t1shfiq <ali.tashfiq4@gmail.com>
2020-12-09 18:52:18 -0500

    Setup Project Files
    Worked on the HTML and CSS files to set up the user interface

---
+++ script.js
@@ -0,0 +1,32 @@
+// API_KEY for maps api
+let API_KEY = "a8e71c9932b20c4ceb0aed183e6a83bb";
+
+/**
+ * Retrieve weather data from openweathermap
+ * https://api.openweathermap.org/data/2.5/weather?q=detroit&appid=a8e71c9932b20c4ceb0aed183e6a83bb&units=imperial
+ */
+
+getWeatherData = (city) => {
+  const URL = "https://api.openweathermap.org/data/2.5/weather";
+
+}
+
+/**
+ * Retrieve city input and get the weather data
+ Uses the promise returned from getWeatherData()
+ */
+searchCity = () => {
+  const city = document.getElementById('city-input').value;
+
+
+}
+
+/**
+ * Show the weather data in HTML
+ */
+
+showWeatherData = (weatherData) => {
+
+  
+}
+
