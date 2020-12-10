caf9391e024595b2dc8293f2679b5c2b7dd34ac5
t1shfiq <ali.tashfiq4@gmail.com>
2020-12-09 22:15:48 -0500

    Finished the app
    Worked on the js file

--- script.js
+++ script.js
@@ -8,6 +8,12 @@ let API_KEY = "a8e71c9932b20c4ceb0aed183e6a83bb";
 
 getWeatherData = (city) => {
   const URL = "https://api.openweathermap.org/data/2.5/weather";
+  const FULL_URL = `${URL}?q=${city}&appid=${API_KEY}&units=imperial`
+
+  const weatherPromise = fetch(FULL_URL);
+  return weatherPromise.then((response) => {
+    return response.json();
+  })
 }
 
 /**
@@ -17,15 +23,21 @@ getWeatherData = (city) => {
 searchCity = () => {
   const city = document.getElementById('city-input').value;
 
-
+  getWeatherData(city)
+  .then((response) => {
+    showWeatherData(response);
+  }).catch((error)=>{
+    console.log(error); 
+  })
 }
 
 /**
  * Show the weather data in HTML
  */
-
 showWeatherData = (weatherData) => {
-
-  
+  document.getElementById("city-name").innerText = weatherData.name;
+  document.getElementById("weather-type").innerText = weatherData.weather[0].main;
+  document.getElementById("temp").innerText = weatherData.main.temp;
+  document.getElementById("min-temp").innerText = weatherData.main.temp_min;
+  document.getElementById("max-temp").innerText = weatherData.main.temp_max;
 }
-
